b9db514d86641de8c251d1e6e9ad66bbb3e72215
p314dO <nicolasgula2023@gmail.com>
2024-07-10 21:40:54 -0300

    add comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import Giscus from '@giscus/react';
 import { Comments as CommentsComponent } from 'pliny/comments'
 import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
@@ -12,11 +12,26 @@ export default function Comments({ slug }: { slug: string }) {
   }
   return (
     <>
-      {loadComments ? (
+      {/* {loadComments ? (
         <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
       ) : (
         <button onClick={() => setLoadComments(true)}>Load Comments</button>
-      )}
+      )} */}
+      <Giscus
+      id="comments"
+      repo="p314dO/Offensive-Mobile"
+      repoId="R_kgDOMS5-NQ"
+      category="General"
+      categoryId="DIC_kwDOMS5-Nc4Cgtak"
+      mapping="pathname"
+      term="Welcome to @giscus/react component!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="dark"
+      lang="en"
+      loading="lazy"
+    />
     </>
   )
 }
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -73,7 +73,7 @@ const siteMetadata = {
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
-      themeURL: '',
+      themeURL: 'preferred_color_scheme',
       // This corresponds to the `data-lang="en"` in giscus's configurations
       lang: 'en',
     },
